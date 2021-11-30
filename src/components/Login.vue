7c85b14447b08d6ab63223f6cade1b4da5572c68
Matt Thomas <tthewmawthomas@gmail.com>
2021-11-30 15:42:04 -0700

    13.2 11/30 axios, signup

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1>Login</h1>
+    <div v-if="this.$route.query.signupsucces" class="alert alert-susess">
+      Thanks for signing up, please log in now.
+    </div>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import axios from "axios";
 
 import myRouter from "./routes.js";
+
+axios.defaults.baseURL =
+  "https://docs.google.com/document/d/1AU329ZjthGx0DoKfadYyZW6vtAcucDRbIXAtYEn5ddI/edit?usp=sharing";
 
 const myApp = createApp(App);
 myApp.use(myRouter);
