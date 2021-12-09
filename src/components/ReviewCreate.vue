3b9cb3b320991309db0aeee67e847dde3c879281
Matt Thomas <tthewmawthomas@gmail.com>
2021-12-09 16:32:56 -0700

    16.1 12/9 create transaction order, redeploy

--- src/components/ReviewCreate.vue
+++ src/components/ReviewCreate.vue
@@ -1,11 +1,81 @@
 <template>
   <div>
-    <h1>Create a Review</h1>
+    <div class="row justify-content-center">
+      <div class="col-lg-10">
+        <div class="card">
+          <div class="card-body">
+            <h4>Place an Order</h4>
+            <form id="review-form" @submit.prevent="submitReview">
+              <div class="mb-3">
+                <label for="summary-input" class="form-label">Review</label
+                ><input
+                  type="text"
+                  row="3"
+                  class="form-control"
+                  id="summary-input"
+                  required=""
+                  v-model="payment"
+                />
+              </div>
+              <button type="submit" class="btn btn-primary">
+                Submit Review</button
+              ><button
+                v-on:click="cancelReview"
+                type="clear"
+                class="btn btn-outline-danger"
+              >
+                Cancel
+              </button>
+
+              <p v-if="errorMessage" class="form-text text-danger">
+                {{ errorMessage }}
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      payment: "",
+      errorMessage: null,
+    };
+  },
+  methods: {
+    submitReview() {
+      let myReview = {
+        payment: this.payment,
+        productID: this.$route.params.pk,
+      };
+
+      axios
+        .post("/orders", myReview, {
+          headers: { Authorization: `Bearer ${this.$store.state.token}` },
+        })
+        .then(() => {
+          this.$router.replace("/account");
+        })
+        .catch(() => {
+          this.errorMessage =
+            "Unable to create a review, please try again later";
+        });
+    },
+    cancelReview() {
+      this.$router.go(-1);
+    },
+  },
+};
 </script>
 
-<style></style>
+<style>
+button {
+  margin-right: 5px;
+}
+</style>
